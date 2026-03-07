65a95a5d3c7feed1dbc7368112c328fd47bc3df2
Khalki Yamine allah <khalkiyamine5@gmail.com>
2026-03-08 00:09:07 +0100

    update admin service

--- src/app/services/admin.service.ts
+++ src/app/services/admin.service.ts
@@ -8,9 +8,11 @@ import { User } from '../all-modules/models/user';
 })
 export class AdminService {
 
-  private baseUrl = 'http://localhost:8081/api/admin';
+  private adminBaseUrl = 'http://localhost:8081/api/admin';
+  private authBaseUrl = 'http://localhost:8081/api/auth';
 
   constructor(private http: HttpClient) { }
+
   private getHeaders(): HttpHeaders {
     const token = localStorage.getItem('token');
 
@@ -22,20 +24,20 @@ export class AdminService {
   }
 
   getPendingCoaches(): Observable<User[]> {
-    return this.http.get<User[]>(`${this.baseUrl}/coaches/pending`, {
+    return this.http.get<User[]>(`${this.adminBaseUrl}/coaches/pending`, {
       headers: this.getHeaders()
     });
   }
 
   approveCoach(id: number): Observable<any> {
-    return this.http.put(`${this.baseUrl}/coaches/${id}/approve`, {}, {
+    return this.http.put(`${this.authBaseUrl}/approve-coach/${id}`, null, {
       headers: this.getHeaders(),
       responseType: 'text'
     });
   }
 
   rejectCoach(id: number): Observable<any> {
-    return this.http.put(`${this.baseUrl}/coaches/${id}/reject`, {}, {
+    return this.http.put(`${this.adminBaseUrl}/coaches/${id}/reject`, null, {
       headers: this.getHeaders(),
       responseType: 'text'
     });
